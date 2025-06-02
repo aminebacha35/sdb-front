0b596e64feb8b369d3019e04aa552dc33f3067f7
Ayoubaitt <ayoub.aitf@gmail.com>
2025-06-02 16:16:45 +0200

    Amélioration de la navbar : espacement du logo, taille de police augmentée

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -3,15 +3,14 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex flex-col md:flex-row justify-between">
         <div class="mb-6 md:mb-0">
-          <h2 class="text-xl font-bold mb-2">GarageRDV</h2>
-          <p class="text-gray-300">Votre garage de confiance depuis 2005</p>
+          <h3 class="text-xl font-bold mb-2"><img src="/sdb.auto.centre.png" class="w-40 ml-11" /></h3>
         </div>
         
         <div class="mb-6 md:mb-0">
           <h3 class="text-lg font-semibold mb-2">Nous contacter</h3>
-          <p class="text-gray-300">123 Avenue des Mécaniciens</p>
-          <p class="text-gray-300">75000 Paris</p>
-          <p class="text-gray-300">Tél: 01 23 45 67 89</p>
+          <p class="text-gray-300">51 Avenue Philippe de Volvire</p>
+          <p class="text-gray-300">35490 Sens-de-bretagne</p>
+          <p class="text-gray-300">Tél: 06 85 53 26 22 - 06 83 78 04 53</p>
           <p class="text-gray-300">Email: contact@garagerdv.fr</p>
         </div>
         
@@ -24,7 +23,7 @@
       </div>
       
       <div class="mt-8 pt-8 border-t border-gray-700 text-center text-gray-400">
-        <p>&copy; {{ new Date().getFullYear() }} GarageRDV. Tous droits réservés.</p>
+        <p>&copy; {{ new Date().getFullYear() }} SDB Auto. Tous droits réservés</p>
       </div>
     </div>
   </footer>
